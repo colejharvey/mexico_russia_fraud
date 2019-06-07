bba6cb9fe680ac73d14029eb675cf31b4beb6e56
Cole Harvey <colejharvey@gmail.com>
2019-06-07 15:01:55 -0400

    Init

---
+++ spikes script russia.R
@@ -0,0 +1,76 @@
+###Trying out Rozenas method of fraud detection (manipulation of vote-shares)
+##Russia 2016
+rm(list=ls())
+library(tidyverse)
+library(spikes)
+
+#Load the data
+
+electoral <- read.csv("C:/Users/Cole/Documents/Grad school/Research topics/All Russian election data/2016 Russia election data full.csv")
+electoral$temp.id <- as.numeric(as.factor(electoral$region.name))
+
+electoral2 <- as_tibble(electoral)  #Set as a tibble
+
+rm(electoral)
+
+#Create container for the results of the spikes program
+
+fraud.estimates <- matrix(NA, nrow=max(electoral2$temp.id), ncol=5) #5 columns: region.id or name, output from spikes, number of precincts where t > n or v > t, total precicnts, and %suspicious * total + additional suspicious
+colnames(fraud.estimates) <- c("region.name", "percent.precincts.suspicious", "additional.precincts.suspicious", "n.precincts", "total.count.suspicious")
+fraud.estimates <- as_tibble(fraud.estimates)
+class(fraud.estimates[[2]])<-"numeric"
+class(fraud.estimates[[3]])<-"numeric"
+class(fraud.estimates[[4]])<-"numeric"
+class(fraud.estimates[[5]])<-"numeric"
+
+#Keep variables needed for spikes (note UR here)--n of ballots cast, ballots cast for a party, and n of registered voters
+
+keepvars <- c("ballots.received", "united.russia", "voter.list") 
+
+#Set up a loop to calculate spikes for each region
+i <- 1
+for(i in 1:max(electoral2$temp.id)){
+  
+  sub.reg <- subset(electoral2, electoral2$temp.id == i) #Pick out one region (6=Donetska)
+  sub.reg.spikes <- sub.reg[keepvars]
+  names(sub.reg.spikes) <- c("t","v", "N")                            #Rename variables to fit spikes
+  if(nrow(sub.reg.spikes) < 100) next #skips empty / small datasets (regions)
+  sub.reg.spikes <- filter(sub.reg.spikes, t <= N & t > v)           #Exclude rows where turnout exceeds 100%
+                                                                     #Drop rows where n of votes for a party exceeds total n of votes
+fraud.estimates[i,3] <- nrow(sub.reg) - nrow(sub.reg.spikes)         #Save number of prima facie suspicious precincts
+  
+  sub.reg.spikes <- filter(sub.reg.spikes, t > 0 & v > 0)            #Drop rows with 0 votes cast to make code work
+  set.seed(1274)                                                     #Set seed for spikes   
+  out <- spikes(sub.reg.spikes)                                      #Runs the spikes program                                                                      #Drop rows with 0 votes for party to make code work
+  fraud.estimates[i,1] <- as.character(sub.reg$region.name)[i]
+  fraud.estimates[i,2] <- as.numeric(summary(out)[1])
+  fraud.estimates[i,4] <- nrow(sub.reg)
+  fraud.estimates[i,5] <- ((fraud.estimates[i,2] / 100) * nrow(sub.reg.spikes)) + fraud.estimates[i,3] 
+  cat("Region estimate complete for region ", as.character(fraud.estimates[i,1]), ". Estimate: ", as.character(fraud.estimates[i,2]))
+  alarm()
+
+}
+  
+###Old code
+for(i in 1:max(electoral$temp.id)){
+  
+  sub.reg <- subset(electoral2, electoral2$temp.id == i) #Pick out one region (6=Donetska)
+  sub.reg.spikes <- sub.reg[keepvars]
+  names(sub.reg.spikes) <- c("t","v", "N") #Rename variables to fit spikes
+  if(nrow(sub.reg.spikes) < 100) next #skips empty / small datasets
+  #sub.reg.spikes[,1] <- ifelse(sub.reg.spikes$t > sub.reg.spikes$N, 
+  #                    sub.reg.spikes$N, sub.reg.spikes$t) #This sets any >100% turnout to 100% so code works
+  sub.reg.spikes <- subset(sub.reg.spikes, sub.reg.spikes$t <= sub.reg.spikes$N)
+  sub.reg.spikes <- subset(sub.reg.spikes, sub.reg.spikes$t > 0) #Drops obs with 0 votes cast to make code work
+  sub.reg.spikes <- subset(sub.reg.spikes, sub.reg.spikes$v > 0) #Drops obs with 0 votes for party to make code work
+  sub.reg.spikes <- subset(sub.reg.spikes, sub.reg.spikes$t > sub.reg.spikes$v) #Drops obs with more votes for party than turnout
+  set.seed(1274)
+  out <- spikes(sub.reg.spikes)  #Runs the spikes program
+  fraud.estimates[i,1] <- as.numeric(summary(out)[1])
+  fraud.estimates[i,2] <- nrow(sub.reg) - nrow(sub.reg.spikes) #Records dropped observations as suspicious
+  fraud.estimates[i,3] <- nrow(sub.reg)
+  fraud.estimates[i,4] <- ((fraud.estimates[i,1] / 100) * nrow(sub.reg.spikes)) + fraud.estimates[i,2] 
+  cat("Region estimate complete for region ", i, "Estimate: ", fraud.estimates[i])
+  alarm()
+  
+}  #Works, but very slow!
